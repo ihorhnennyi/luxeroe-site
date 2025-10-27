8acc4f1f8b19f2621517f2e0a73f875b9ffaf5bb
ihorhnennyi <ihorhnennyi@gmail.com>
2025-10-27 14:42:43 +0200

    update mobile menu 1

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,11 +1,20 @@
 export type SocialLink = {
   label: string
   href: string
-  icon: 'instagram' | 'facebook' | 'telegram'
+  icon: 'instagram' | 'facebook' | 'telegram' | 'tiktok'
 }
 
 export const SOCIALS: SocialLink[] = [
-  { label: 'Instagram', href: 'https://instagram.com/', icon: 'instagram' },
-  { label: 'Facebook', href: 'https://facebook.com/', icon: 'facebook' },
-  { label: 'Telegram', href: 'https://t.me/', icon: 'telegram' }
+  { label: 'Instagram', href: 'https://www.instagram.com/luxe.roe', icon: 'instagram' },
+  {
+    label: 'Facebook',
+    href: 'https://www.facebook.com/profile.php?id=61575086133995',
+    icon: 'facebook'
+  },
+  { label: 'Telegram', href: 'https://t.me/luxeroem', icon: 'telegram' },
+  {
+    label: 'TikTok',
+    href: 'https://www.tiktok.com/@luxe.roe?_t=ZM-90toSRjMAoq&_r=1',
+    icon: 'tiktok'
+  }
 ]
